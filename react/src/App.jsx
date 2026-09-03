0c22a2a4e5c6b4caa757b3a075d97ab37f0a98d7
miorii7 <milkaway227@gmail.com>
2026-09-03 04:24:28 +0000

    1

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,8 +1,39 @@
+import { useState } from "react"
+import Greeting from "./components/Greeting"
+
 const App = () => {
-  const username = "ktkv419"
+
+  const [counter, setCounter] = useState(0)
+
+
+  const username = "miorii7"
+
+
+
+  //const handleMinus = () => {
+  //  setCounter(counter - 1 )
+  //
+//
+  //}
+
+  //const handlePlus = () => {
+  //setCounter(counter + 1 )
+
+  const handleCounter = (diff) => {
+    setCounter((val) => val + diff)
+
+  }
 
   return (
-    <h1>С подключением, {username}</h1>
+    <>
+
+    <Greeting username = {username} />
+    <div>
+      <button onClick={() =>handleCounter(-1)}>-</button>
+      <span>{counter}</span>
+      <button onClick={() => handleCounter(1)}>+</button>
+    </div>
+    </>
   )
 }
 
---
+++ react/src/components/Greeting.jsx
@@ -0,0 +1,6 @@
+const Greeting = ({username}) => {
+    return <h1>С подключением, {username}</h1>
+
+}
+
+export default Greeting
